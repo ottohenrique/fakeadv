556b19baddc1d47565560ad22d0f96face78137a
Otto Henrique <ohorvath@sp.r7.com.br>
2011-05-04 16:27:41 -0300

    cadastra formatos

--- fakeadv.rb
+++ fakeadv.rb
@@ -19,63 +19,51 @@ resp, data = http.post '/login', login_data, {'Cookie'=>cookie_id}
 
 puts "Conectado..."
 
-puts resp['location']
-
+# incluir secoes
 
 resp, data = http.get '/secao/novo', {'Cookie'=>cookie_id}
 
 doc = Hpricot(data)
 site_id = (doc/"#site/option[text()='Esportes']").attr('value')
 
-f_sessoes = File.open('sessoes-fussbal.txt')
-f_sessoes.each do |l|
-	nome = l.split '/'
-	nome = nome[1]
-	
-	insert  = "secao.idFerramenta=237"
-	insert += "&secao.nome=futebol_campeonato_" + nome.gsub(/-/,'_')
-	insert += "&secao.site.id=" + site_id 
-	insert += "&secao.site.prefixoUrl=http://esportes.r7.com"
-	insert += "&secao.url=/futebol/"+nome+"/minutoaminuto"
+#f_sessoes = File.open('sessoes-fussbal.txt')
+#f_sessoes.each do |l|
+#	nome = l.split '/'
+	#nome = nome[1]
+#	
+	#insert  = "secao.idFerramenta=237"
+#	insert += "&secao.nome=futebol_campeonato_" + nome.gsub(/-/,'_')
+#	insert += "&secao.site.id=" + site_id 
+#	insert += "&secao.site.prefixoUrl=http://esportes.r7.com"
+#	insert += "&secao.url=/futebol/"+nome+"/minutoaminuto"
 
-	insert = URI.escape insert
-	http.post '/secao/cadastrar', insert, {'Cookie'=>cookie_id}
+#	insert = URI.escape insert
+#	http.post '/secao/cadastrar', insert, {'Cookie'=>cookie_id}
 
-	puts "Sessao adicionada: #{nome}\n"
+#	puts "Sessao adicionada: #{nome}\n"
+#end
+
+
+# cadastra formatos
+
+resp, data = http.get '/secoesDoSite/'+site_id, {'Cookie'=>cookie_id}
+doc = Hpricot(data)
+count = 0
+(doc/"#secao/option").each do |el|
+    if(el.innerHTML.match /futebol_campeonato/)
+        section_id = el['value']
+        
+        insert  = "formato.formatoNome.id=31"
+        insert += "&formato.formatoNome.tipoPublicidade.id=1"
+        insert += "&formato.idFerramenta=3"
+        insert += "&formato.secao.id=" + section_id
+        insert += "&site.id=" + site_id
+        
+        insert = URI.escape insert
+        http.post '/formato/cadastra', insert, {'Cookie'=>cookie_id}
+        
+        puts "Formato cadastrado em " + el.innerHTML
+        count = count+1
+    end
 end
-
-/formato/cadastra
-
-formato.altura	
-formato.formatoNome.id	31
-formato.formatoNome.tipoP...	1
-formato.googleAdChannel	
-formato.googleAdFormat	
-formato.googleAdOutput	
-formato.googleAdSection	
-formato.googleAdType	
-formato.googleAdtest	
-formato.googleColorBg	
-formato.googleColorBorder	
-formato.googleColorLine	
-formato.googleColorLink	
-formato.googleColorText	
-formato.googleColorUrl	
-formato.googleEncoding	
-formato.googleHints	
-formato.googleImageSize	
-formato.googleLanguage	
-formato.googleMaxNumAds	
-formato.googlePageUrl	
-formato.googleSafe	
-formato.googleSkip	
-formato.idFerramenta	3
-formato.largura	
-formato.peso	
-formato.posicao	
-formato.secao.id	4342
-site.id	2174
-
-#site/option[text()=Esportes]
-#secao
-
+puts "total de formatos: #{count}"
